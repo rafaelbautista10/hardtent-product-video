fe8cdf4639ad78d94aa417f7508a25fa000b6c33
andresOutliant <143036733+andresOutliant@users.noreply.github.com>
2024-03-11 21:22:15 -0400

    Update and rename HardCamp-Checkout7.js to HardCamp-Checkout8.js

--- HardCamp-Checkout8.js
+++ HardCamp-Checkout8.js
@@ -614,3 +614,12 @@ window.addEventListener('resize', adjustDescriptionText);
       });
   });
 });
+
+
+(function($) {
+    $.fx.prototype.curOriginal = $.fx.prototype.cur;
+    $.fx.prototype.cur = function(force) {
+        if ($(this.elem).is("body") && this.prop == "scrollTop") return $(window).scrollTop();
+        return this.curOriginal(force);
+    }
+})(jQuery);
