03ffa1aca16f81d4fac1db73ee407b4802adcca4
andresOutliant <143036733+andresOutliant@users.noreply.github.com>
2024-03-15 17:34:01 -0400

    Update and rename scroll-code29.js to scroll-code30.js

--- scroll-code30.js
+++ scroll-code30.js
@@ -36,7 +36,7 @@ function reverseVideo() {
     // Use window width to determine the values for mobile vs desktop
     const isMobile = window.innerWidth < 768;
     const reverseInterval = isMobile ? 30 : 50; // 30 for mobile, 50 for desktop
-    const stepBack = isMobile ? 0.05 : 0.02; // 0.05 for mobile, 0.02 for desktop
+    const stepBack = isMobile ? 0.04 : 0.02; // 0.05 for mobile, 0.02 for desktop
 
     if (video.playbackRate !== 1) {
         video.playbackRate = 1;
